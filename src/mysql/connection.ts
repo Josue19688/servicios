0618842bf26984b683653ab6db5c3553baf22332
Josue-dev <52864843+Josue-dev@users.noreply.github.com>
2023-04-23 12:57:46 -0600

    verificando logs

--- src/mysql/connection.ts
+++ src/mysql/connection.ts
@@ -5,7 +5,7 @@ import  {Sequelize} from 'sequelize';
 const db = new Sequelize(`${process.env.NAME_DATABASE}`,`${process.env.USER_DATABASE}`,`${process.env.PASS_DATABASE}`,{
     host:process.env.HOST_DATABASE,
     dialect:'mysql',
-    port:Number(process.env.HOST_DATABASE),
+    port:Number(process.env.MYSQLPORT),
     pool:{
         max:5,
         min:0,
